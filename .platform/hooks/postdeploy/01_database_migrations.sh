4ae8d428f25008504a3c347360fc7d86f5d630fc
Brandon Lim <brandonlim.lim@gmail.com>
2020-10-12 22:14:39 +1100

    EB: Update post deploy database migration script

--- .platform/hooks/postdeploy/01_database_migrations.sh
+++ .platform/hooks/postdeploy/01_database_migrations.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# This is a single container setup, the first (and only) container contains the Laravel project
-CONTAINER_ID=$(docker ps -a -q | head -n 1)
-
 # Run Laravel's migration command
-docker exec $CONTAINER_ID php artisan migrate --force
+docker exec app php artisan migrate --force
